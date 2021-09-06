364b7b5c5b62daed78904d65fd2fc8dadedcbb87
juliocslima <lima.julio.cs@gmail.com>
2021-09-06 17:01:37 -0300

    Project created

---
+++ backend/src/test/java/br/com/p4s/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.p4s.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
